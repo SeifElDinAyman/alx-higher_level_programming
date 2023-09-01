c50ec4eaec2d83dd895cfc5433f6017a77f06be0
Seif El-Din <SeifElDinAyman77@gmail.com>
2023-09-01 15:46:45 +0300

    MSG

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,32 +1,13 @@
 #!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status.
 """
-This script fetches internet resources using the requests package.
-It demonstrates how to make HTTP GET requests, decode the response,
-and manipulate data from an external service.
-"""
-
 import requests
 
-def fetch_url(url):
-    """
-    Fetches and prints the content of a URL.
-    
-    Args:
-        url (str): The URL to fetch.
-    """
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        content_type = response.headers.get('content-type', 'Unknown')
-        
-        print("Body response:")
-        print(f"\t- type: {type(response.text)}")
-        print(f"\t- content: {content_type}\n")
-        print(response.text)
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    url_to_fetch = 'https://alx-intranet.hbtn.io/status'
-    fetch_url(url_to_fetch)
+    url = "https://alx-intranet.hbtn.io/status"
 
+    r = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
