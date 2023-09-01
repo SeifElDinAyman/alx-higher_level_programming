1c77ace165a2297d3122f84c7b7b2a7380db1c44
Seif El-Din <SeifElDinAyman77@gmail.com>
2023-09-01 15:39:47 +0300

    MSG

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""fetches https://alx-intranet.hbtn.io/status."""
+"""fetches https://alx-intranet.hbtn.io/status."""   
 import requests
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    r = requests.get(url)
-    
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+url = 'https://alx-intranet.hbtn.io/status'
+response = requests.get(url)
+
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
+
