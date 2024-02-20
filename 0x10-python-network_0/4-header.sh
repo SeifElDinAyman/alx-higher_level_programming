4d3d3ce593327caf09ec1a71feb0ccd487b3817a
SeifElDinAyman <117994578+SeifElDinAyman@users.noreply.github.com>
2024-02-21 00:21:31 +0200

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -X GET -H "X-School-User-Id:98" "${1}" ; echo ""
+
+# Check if the script is provided with a URL argument
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# URL argument
+url=$1
+
+# Sending GET request with X-School-User-Id header
+curl -X GET -H "X-School-User-Id: 98" "$url"
